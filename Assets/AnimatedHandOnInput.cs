dccff9ee7981f34be6e9a476fa0652cc6bbb28da
Thomas Roig <thomas.roig@etu.sorbonne-universite.fr>
2024-02-07 17:02:07 +0100

    je teste

--- Assets/AnimatedHandOnInput.cs
+++ Assets/AnimatedHandOnInput.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class AnimatedHandOnInput : MonoBehaviour
-{
+
+public class AnimatedHandOnInput : MonoBehaviour{
+    // Capture boutton avant de la manette
     public InputActionProperty pinchAnimationAction;
+    // Capture boutton lateral manette
+    public InputActionProperty gripAnimationAction;
+    //Permet de linker l'animation et de parameter les animations
+    public Animator handAnimator;
+
+   
+
     // Start is called before the first frame update
     void Start(){
         
@@ -13,6 +21,10 @@ public class AnimatedHandOnInput : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        
+        //get value du boutton avant presse
+        float tvalue = pinchAnimationAction.action.ReadValue<float>();
+        //Gere l'animation
+        handAnimator.SetFloat("Trigger",tvalue);
+
     }
 }
