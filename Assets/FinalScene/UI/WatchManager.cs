4f7bda5830a6be3321f01c976d8fd90fcce291bd
Kvn00000 <94387120+Kvn00000@users.noreply.github.com>
2024-06-25 09:35:34 +0200

    resize UI

--- Assets/FinalScene/UI/WatchManager.cs
+++ Assets/FinalScene/UI/WatchManager.cs
@@ -12,17 +12,19 @@ public class WatchManager : MonoBehaviour
     public GameObject menuLeft;
     public GameObject left;
 
-    public Vector3 positionOffsetLeft = new Vector3(0.52f, -0.1f, -0.24f);
+    public Vector3 positionOffsetLeft = new Vector3(-0.04f, -0.02f, 0f);
     public Vector3 rotationOffsetLeft = new Vector3(90, 0, 0);
 
-    public Vector3 positionOffsetRight = new Vector3(-0.52f, -0.1f, 0.06f);
+    public Vector3 positionOffsetRight = new Vector3(0.04f, 0.02f, 0f);
     public Vector3 rotationOffsetRight = new Vector3(90, 180, 0);
 
     [Header("Spray and Positions")]
     public GameObject bottleSpray;
 
-    public Vector3 SprayPositionOffset = new Vector3(-0.52f, -0.1f, 0.06f);
-    public Vector3 SprayrotationOffset = new Vector3(90, 180, 0);
+    public Vector3 SprayPositionOffsetLeft = new Vector3(0f, 0.5f, 0.05f);
+    public Vector3 SprayrotationOffsetLeft = new Vector3(0, 0, 0);
+    public Vector3 SprayPositionOffsetRight = new Vector3(0f, -0.05f, -0.05f);
+    public Vector3 SprayrotationOffsetRight = new Vector3(0, 0, 0);
 
     [Header("Controllers")] 
 
@@ -43,16 +45,20 @@ public class WatchManager : MonoBehaviour
         if(left.activeSelf == true){
             menuLeft.transform.position = LeftController.transform.position + LeftController.transform.TransformVector(positionOffsetLeft);
             menuLeft.transform.rotation =  LeftController.transform.rotation * Quaternion.Euler(rotationOffsetLeft);
-            bottleSpray.transform.position = RightController.transform.position;
-            bottleSpray.transform.rotation = RightController.transform.rotation;
+
+            //Quand le menu est sur la gauche le spray doit etre sur la droite
+            bottleSpray.transform.position = RightController.transform.position + RightController.transform.TransformVector(SprayPositionOffsetRight);
+            bottleSpray.transform.rotation = RightController.transform.rotation * Quaternion.Euler(SprayrotationOffsetRight);
             leftSpawner.enabled = false;
             RightSpawner.enabled = true;
 
         }else{
             menuLeft.transform.position = RightController.transform.position + RightController.transform.TransformVector(positionOffsetRight);
             menuLeft.transform.rotation =  RightController.transform.rotation * Quaternion.Euler(rotationOffsetRight);
-            bottleSpray.transform.position = LeftController.transform.position;
-            bottleSpray.transform.rotation = LeftController.transform.rotation;
+
+            //Menu a droite donc spray a gauche
+            bottleSpray.transform.position = LeftController.transform.position + LeftController.transform.TransformVector(SprayrotationOffsetLeft);
+            bottleSpray.transform.rotation = LeftController.transform.rotation * Quaternion.Euler(SprayrotationOffsetLeft);
             leftSpawner.enabled = true;
             RightSpawner.enabled = false;
             
