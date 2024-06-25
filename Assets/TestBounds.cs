7b62e34aa2bde574a2546c02c60323df162c97b2
FirionSun <ewokthr@outlook.fr>
2024-06-25 14:29:52 +0100

    Essai detection avec bounds

---
+++ Assets/TestBounds.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BoxCollider box = this.GetComponent<BoxCollider>();
+        Vector3 center=box.bounds.center;
+        Debug.Log("EXTENTS "+box.bounds.extents);
+    }
+}
