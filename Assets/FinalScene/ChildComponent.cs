915467cdb845713493bc45bdd801d2a2b5d8ebfe
Kvn00000 <94387120+Kvn00000@users.noreply.github.com>
2024-06-26 13:10:21 +0200

    nouveau cube pour grab

--- Assets/FinalScene/ChildComponent.cs
+++ Assets/FinalScene/ChildComponent.cs
@@ -2,24 +2,249 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChildComponent : MonoBehaviour
-{
+public class ChildComponent : MonoBehaviour{
+
+    private MeshFilter _meshFilter;
+    private Mesh myMesh;
+    public Vector3[] myVertices;
+
+
+    private int[] myTriangles;
+
+
+    //Child mesh
+    public MeshFilter meshFilterTop;
+    public MeshFilter meshFilterFront;
+    public MeshFilter meshFilterLeft;
+    public MeshFilter meshFilterRight;
+    public MeshFilter meshFilterBack;
+    public MeshFilter meshFilterBottom;
+
+    //Child Transform
+    private Transform transformTop;
+    private Transform transformFront;
+    private Transform transformLeft;
+    private Transform transformRight;
+    private Transform transformBack;
+    private Transform transformBottom;
+
+    //Child Transform
+    public BoxCollider colliderTop;
+    public BoxCollider colliderFront;
+    public BoxCollider colliderLeft;
+    public BoxCollider colliderRight;
+    public BoxCollider colliderBack;
+    public BoxCollider colliderBottom;
+
+
+    void Awake(){
+        _meshFilter = GetComponent<MeshFilter>();
+        myMesh = _meshFilter.mesh;
+        myVertices = new Vector3[8]{
+            new Vector3(-0.5f, -0.5f, -0.5f),
+            new Vector3(-0.5f, 0.5f, -0.5f),
+            new Vector3(0.5f, -0.5f, -0.5f),
+            new Vector3(0.5f, 0.5f, -0.5f),
+            new Vector3(-0.5f, -0.5f, 0.5f),
+            new Vector3(-0.5f, 0.5f, 0.5f),
+            new Vector3(0.5f, -0.5f, 0.5f),
+            new Vector3(0.5f, 0.5f, 0.5f)
+        };
+
+        myTriangles = new int[36]{
+            //Add the triangles clockwise
+            //Front
+            1,2,0,
+            1,3,2,
+            //Top
+            5,3,1,
+            5,7,3,
+            //left
+            5,0,4,
+            5,1,0,
+            //Right
+            3,6,2,
+            3,7,6,
+            //Back
+            7,4,6,
+            7,5,4,
+            //Bottom
+            0,6,4,
+            0,2,6
+        };
+
+        myMesh.vertices = myVertices;
+        myMesh.triangles = myTriangles;
+    }
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         foreach (Transform child in transform){
+            if (child.gameObject.GetComponent<MeshFilter>() == null){
+                switch(child.name){
+                    case "Top":
+                        transformTop = child;
+                        meshFilterTop = child.gameObject.AddComponent<MeshFilter>();
+                        colliderTop = child.gameObject.GetComponent<BoxCollider>();
+                        int[] triangleTop = new int[6]{
+                            5,3,1,
+                            5,7,3,
+                        };
+                        Vector3[] verticeTop = new Vector3[4]{
+                            myVertices[5],
+                            myVertices[3],
+                            myVertices[1],
+                            myVertices[7]
+                        };
 
-            if (child.gameObject.GetComponent<MeshFilter>() == null)
-            {
-                child.gameObject.AddComponent<MeshFilter>();
+
+                        InitOneChild(triangleTop, meshFilterTop, transformTop,CalculateMeshCenter(verticeTop) ,CalculateMeshSize(verticeTop));
+                        break;
+                    case "Front":
+                        transformFront = child;
+                        meshFilterFront = child.gameObject.AddComponent<MeshFilter>();
+                        colliderFront = child.gameObject.GetComponent<BoxCollider>();
+
+                        int[] triangleFront = new int[6]{
+                            1,2,0,
+                            1,3,2,
+                        };
+
+                        Vector3[] verticeFront = new Vector3[4]{
+                            myVertices[1],
+                            myVertices[2],
+                            myVertices[0],
+                            myVertices[3]
+                        };
+                        InitOneChild(triangleFront, meshFilterFront, transformFront, CalculateMeshCenter(verticeFront), CalculateMeshSize(verticeFront));
+                        break;
+                    case "Left":
+                        transformLeft = child;
+                        meshFilterLeft = child.gameObject.AddComponent<MeshFilter>();
+                        colliderLeft = child.gameObject.GetComponent<BoxCollider>();
+
+                        int[] triangleLeft = new int[6]{
+                            5,0,4,
+                            5,1,0,
+                        };
+
+                        Vector3[] verticeLeft = new Vector3[4]{
+                            myVertices[5],
+                            myVertices[0],
+                            myVertices[4],
+                            myVertices[1]
+                        };
+                        InitOneChild(triangleLeft, meshFilterLeft, transformLeft, CalculateMeshCenter(verticeLeft), CalculateMeshSize(verticeLeft));
+                        break;
+                    case "Right":
+                        transformRight = child;
+                        meshFilterRight = child.gameObject.AddComponent<MeshFilter>();
+                        colliderRight = child.gameObject.GetComponent<BoxCollider>();
+
+                        int[] triangleRight = new int[6]{
+                            3,6,2,
+                            3,7,6,
+                        };
+
+                        Vector3[] verticeRight = new Vector3[4]{
+                            myVertices[3],
+                            myVertices[6],
+                            myVertices[2],
+                            myVertices[7]
+                        };
+                        InitOneChild(triangleRight, meshFilterRight, transformRight, CalculateMeshCenter(verticeRight), CalculateMeshSize(verticeRight));
+                        break;
+                    case "Back":
+                        transformBack = child;
+                        meshFilterBack = child.gameObject.AddComponent<MeshFilter>();
+                        colliderBack = child.gameObject.GetComponent<BoxCollider>();
+                        
+                        int[] triangleBack = new int[6]{
+                            7,4,6,
+                            7,5,4,
+                        };
+
+                        Vector3[] verticeBack = new Vector3[4]{
+                            myVertices[7],
+                            myVertices[4],
+                            myVertices[6],
+                            myVertices[5]
+                        };
+                        InitOneChild(triangleBack, meshFilterBack, transformBack, CalculateMeshCenter(verticeBack), CalculateMeshSize(verticeBack));
+                        break;
+                    case "Bottom":
+                        transformBottom = child;
+                        meshFilterBottom = child.gameObject.AddComponent<MeshFilter>();
+                        colliderBottom = child.gameObject.GetComponent<BoxCollider>();
+
+                        int[] triangleBottom = new int[6]{
+                            0,6,4,
+                            0,2,6
+                        };
+
+                        Vector3[] verticeBottom = new Vector3[4]{
+                            myVertices[0],
+                            myVertices[6],
+                            myVertices[4],
+                            myVertices[2]
+                        };
+                        InitOneChild(triangleBottom, meshFilterBottom, transformBottom, CalculateMeshCenter(verticeBottom), CalculateMeshSize(verticeBottom));
+                        break;
+                }
+
+                Debug.Log(" les childs : " + child.GetType());
             }
-
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    void InitOneChild(int[] triangles, MeshFilter m, Transform t, Vector3 center, Vector3 size){
+        Mesh meshs = new Mesh();
+        meshs.vertices = myVertices;
+        meshs.triangles = triangles;
+        m.mesh = meshs;
+        BoxCollider collider = t.gameObject.GetComponent<BoxCollider>();
+        // collider.center = center;
+        // collider.size = size;
     }
+
+
+    public Vector3 CalculateMeshSize(Vector3[] vertices)
+    {
+        Vector3 min = vertices[0];
+        Vector3 max = vertices[0];
+
+        foreach (Vector3 vertex in vertices)
+        {
+            min = Vector3.Min(min, vertex);
+            max = Vector3.Max(max, vertex);
+        }
+
+        Vector3 size = max - min;
+        return size;
+    }
+
+
+    public Vector3 CalculateMeshCenter(Vector3[] vertices)
+    {
+        Vector3 center = Vector3.zero;
+        
+        foreach (Vector3 vertex in vertices)
+        {
+            center += vertex;
+        }
+        center /= vertices.Length;
+        return center;
+    }
+
+    
+    public void updateVertices(Vector3[] newVertice, Vector3[] colliderVertice, MeshFilter m, BoxCollider bc){
+        
+
+        m.mesh.vertices = newVertice;
+        bc.size = CalculateMeshSize(colliderVertice);
+        bc.center = CalculateMeshCenter(colliderVertice);
+
+    }
+    
+
 }
--- Assets/FinalScene/TwoHandGrabDetector.cs
+++ Assets/FinalScene/TwoHandGrabDetector.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class TwoHandGrabDetector : MonoBehaviour
 {
@@ -9,12 +10,16 @@ public class TwoHandGrabDetector : MonoBehaviour
     private XRGrabInteractable grabInteractable;
     private List<XRBaseInteractor> interactors = new List<XRBaseInteractor>();
 
+    private ChildComponent childs;
     private MeshFilter mesh;
+
+    public InputActionProperty grabAction;
 
     void Awake()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
         mesh = GetComponent<MeshFilter>();
+        childs = GetComponent<ChildComponent>();
     }
 
     void OnEnable()
@@ -36,44 +41,7 @@ public class TwoHandGrabDetector : MonoBehaviour
             interactors.Add(args.interactor);
         }
 
-        if (interactors.Count == 2){
-            Debug.Log("Object grabbed with both hands.");
-            
-            // foreach (var collider in args.interactable.colliders)
-            // {
-            //     string colliderName = collider.name;
-            //     Debug.Log("Collider grabbé : " + colliderName);
-                
-            // }
-            // Debug.Log("???????");
-
-            // XRBaseInteractable interactable = args.interactable;
-
-            // // Parcours des colliders de l'interactable
-            // foreach (var collider in interactable.colliders)
-            // {
-            //     // Vérification si le collider est touché par le rayon du XR Ray Interactor
-            //     RaycastHit hitInfo;
-            //     bool isHit = xrInteractor.GetCurrentRaycastHit(out hitInfo);
-
-            //     if (isHit && hitInfo.collider == collider)
-            //     {
-            //         Debug.Log("Collider en collision avec le ray : " + collider.name);
-            //         // Faites quelque chose avec le collider s'il est touché par le rayon
-            //     }
-            // }
-
-
-            if (xrInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit))
-            {
-                // Affichez le nom du collider touché
-                Debug.Log("Collider touché : " + hit.collider.name);
-
-                // Affichez le nom de l'objet parent du collider touché
-                Debug.Log("Parent de l'objet touché : " + hit.collider.transform.parent.name);
-            }
-
-        }
+        
     }
 
     private void OnRelease(SelectExitEventArgs args)
@@ -83,50 +51,59 @@ public class TwoHandGrabDetector : MonoBehaviour
         }
 
         if (interactors.Count < 2){
-            Debug.Log(mesh.mesh.vertices.Length);
+            // Debug.Log(mesh.mesh.vertices.Length);
 
-            for (int i = 0; i < mesh.mesh.vertices.Length; i++){
-                Debug.Log($"Vertex {i}: {mesh.mesh.vertices[i]} et en global ::: {transform.TransformPoint(mesh.mesh.vertices[i])}");
-            }
+            // for (int i = 0; i < mesh.mesh.vertices.Length; i++){
+            //     Debug.Log($"Vertex {i}: {mesh.mesh.vertices[i]} et en global ::: {transform.TransformPoint(mesh.mesh.vertices[i])}");
+            // }
             
 
-            Debug.Log(mesh.mesh.triangles.Length);
-            for(int i = 0; i < mesh.mesh.triangles.Length; i++){
-                Debug.Log($"Trianggle {i}: {mesh.mesh.triangles[i]}");
+            // Debug.Log(mesh.mesh.triangles.Length);
+            // for(int i = 0; i < mesh.mesh.triangles.Length; i++){
+            //     Debug.Log($"Trianggle {i}: {mesh.mesh.triangles[i]}");
                 
-            }
+            // }
 
-            Debug.Log("Object is no longer grabbed with both hands.");
+            // Debug.Log("Object is no longer grabbed with both hands.");
 
             // Your logic when the object is no longer grabbed with both hands
         }
     }
 
+    void Update(){
+        if (interactors.Count == 2){
+            if (xrInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit)){
+                Debug.Log("Collider touché : " + hit.collider.name);
 
-    private void DetectFace(XRRayInteractor rayInteractor)
-    {
-        if (rayInteractor.TryGetHitInfo(out Vector3 hitPosition, out Vector3 hitNormal, out int _, out bool _))
-        {
-            string face = GetCubeFace(transform.TransformDirection(hitNormal));
-            Debug.Log("Face touchée : " + face);
+                switch(hit.collider.name){
+                    case "Top":
+                        Vector3[] newTopVertice = childs.myVertices;
+
+                        // childs.updateVertices();
+                        break;
+                    
+                    case "Front":
+                        break;
+                    
+                    case "Left":
+                        break;
+
+                    case "Right":
+                        break;
+
+                    case "Back":
+                        break;
+
+                    case "Bottom":
+                        break;
+                }
+            }
+
         }
+        
+
     }
 
-    private string GetCubeFace(Vector3 hitNormal)
-    {
-        if (hitNormal == Vector3.up)
-            return "Haut";
-        else if (hitNormal == Vector3.down)
-            return "Bas";
-        else if (hitNormal == Vector3.left)
-            return "Gauche";
-        else if (hitNormal == Vector3.right)
-            return "Droite";
-        else if (hitNormal == Vector3.forward)
-            return "Devant";
-        else if (hitNormal == Vector3.back)
-            return "Derrière";
-        else
-            return "Inconnue " + hitNormal;
-    }
+
+    
 }
