b244b70097ef9eda7e8d07bd1e4b91a365d16486
FirionSun <ewokthr@outlook.fr>
2024-06-27 10:11:57 +0100

    Ajout toolbox resize

--- Assets/TestBounds.cs
+++ Assets/TestBounds.cs
@@ -13,7 +13,8 @@ public class TestBounds : MonoBehaviour
     public Vector3 left;
     public Vector3 right;
     public BoxCollider box;
-
+    public float resizeAmount=1.5f;
+    private bool inverse = false;
     private Quaternion topQuat;
     private Rigidbody rb;
     // Start is called before the first frame update
@@ -38,21 +39,22 @@ public class TestBounds : MonoBehaviour
 
         Debug.Log("EXTENTS " + box.bounds.extents);
         Debug.Log("TOP is " + top+ " bottom is "+bottom);
+        resizeCube(resizeAmount, "x");
         //Debug.Log("Front is " + front + " back is " + back);
         //Debug.Log("Right is " + right + " left is " + left);
-        Debug.Log("center " + center + " other center " + this.GetComponent<BoxCollider>().bounds);
-        Debug.Log(" Quater is " + this.transform.rotation);
+        //Debug.Log("center " + center + " other center " + this.GetComponent<BoxCollider>().bounds);
+        //Debug.Log(" Quater is " + this.transform.rotation);
         Debug.DrawLine(center, top, Color.red);
         Vector3 rot = this.transform.rotation.eulerAngles;
         Vector3 newTop = RotatePointAroundPivot(top,center,rot) ;
         Debug.DrawLine(center, newTop, Color.green);
-        Quaternion test = Quaternion.FromToRotation(top, newTop);
+       Quaternion test = Quaternion.FromToRotation(top, newTop);
         Quaternion test2 = Quaternion.FromToRotation(center, newTop);
-        //Quaternion t3 = Quaternion.LookRotation(newTop, top);
+        Quaternion t3 = Quaternion.LookRotation(newTop, top);
         //Debug.Log(" Test 1: " + test + " test 2 : " + test2);
         //Debug.Log(" Rotate Test : " + this.transform.rotation.eulerAngles + "  Via newTop = " + test.eulerAngles + " t2 : " + test2.eulerAngles);
         //Debug.Log("T3 " + RotatePointAroundPivot(newTop,center,Quaternion.Inverse(this.transform.rotation).eulerAngles)) ;
-        Debug.Log("Box center "+this.transform.TransformPoint(box.center)+" Rigid Center "+this.transform.TransformPoint(rb.centerOfMass));
+        //Debug.Log("Box center "+this.transform.TransformPoint(box.center)+" Rigid Center "+this.transform.TransformPoint(rb.centerOfMass));
     }
     private Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles){
         Vector3 dir = point - pivot; // get point direction relative to pivot
@@ -61,4 +63,53 @@ public class TestBounds : MonoBehaviour
         return nPoint; // return it
 
     }
+
+   private void resizeCube(float amount, string axis)
+   {
+        Debug.Log("Here");
+        switch (axis)
+        {
+            case "x":
+                if (!inverse)
+                {
+                    Debug.Log("ICI");
+                    this.transform.position = new Vector3(this.transform.position.x+(amount/2), this.transform.position.y, this.transform.position.z);
+                    this.transform.localScale = new Vector3(this.transform.localScale.x+amount, this.transform.localScale.y, this.transform.localScale.z);
+                }
+                else
+                {
+                    
+                    this.transform.position = new Vector3(this.transform.position.x-(amount/2), this.transform.position.y, this.transform.position.z);
+                    this.transform.localScale = new Vector3(this.transform.localScale.x-amount, this.transform.localScale.y, this.transform.localScale.z);
+                }
+                break;
+            case "y":
+                if (!inverse)
+                {
+                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + (amount / 2), this.transform.position.z);
+                    this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y+amount, this.transform.localScale.z);
+                }
+                else
+                {
+                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y-(amount / 2), this.transform.position.z);
+                    this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y-amount, this.transform.localScale.z);
+                }
+                break;
+            case "z":
+                if (!inverse)
+                {
+                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + (amount / 2));
+                    this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z + amount);
+                }
+                else
+                {
+                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z- (amount / 2));
+                    this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z- amount);
+                }
+                break;
+            default:
+                break;
+        
+        }
+   }
 }
