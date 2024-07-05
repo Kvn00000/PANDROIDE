05205dec3c00bad0a32bafa5c17f9f965a395749
Kvn00000 <94387120+Kvn00000@users.noreply.github.com>
2024-07-05 13:55:10 +0200

    fade out

--- Assets/FinalScene/Objects/FadeOutScript.cs
+++ Assets/FinalScene/Objects/FadeOutScript.cs
@@ -16,37 +16,37 @@ public class FadeOut : MonoBehaviour
     public float initialAlpha = 1f;
     public float fadeDuration = 5f; // 5 secondes pour fade out
 
-    // void Awake(){
-    //     grabInteractable = GetComponent<XRGrabInteractable>();
-    //     renderer = gameObject.GetComponent<Renderer>();
-    //     color = renderer.material.color;
-    // }
+    void Awake(){
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        renderer = gameObject.GetComponent<Renderer>();
+        color = renderer.material.color;
+    }
 
-    // void Update(){
+    void Update(){
 
-    //     if(isFadingOut == true){
-    //         elapsedTime += Time.deltaTime;
-    //         color.a = Mathf.Lerp(initialAlpha, 0f, elapsedTime / fadeDuration);
-    //         renderer.material.color = color;
+        if(isFadingOut == true){
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Lerp(initialAlpha, 0f, elapsedTime / fadeDuration);
+            renderer.material.color = color;
 
-    //         if (elapsedTime >= fadeDuration){
-    //             isFadingOut = false;
-    //             elapsedTime = 0f;
-    //             Destroy(gameObject);
-    //         }
-    //     }
-    // }
-    // void OnEnable(){
-    //     grabInteractable.selectEntered.AddListener(OnGrab);
-    // }
+            if (elapsedTime >= fadeDuration){
+                isFadingOut = false;
+                elapsedTime = 0f;
+                Destroy(gameObject);
+            }
+        }
+    }
+    void OnEnable(){
+        grabInteractable.selectEntered.AddListener(OnGrab);
+    }
 
-    // // Start is called before the first frame update
-    // private void OnGrab(SelectEnterEventArgs args){
-    //     if(isFadingOut == true){
-    //         isFadingOut = false;
-    //         elapsedTime = 0f;
-    //         color.a = 1f;
-    //         renderer.material.color = color;
-    //     }
-    // }
+    // Start is called before the first frame update
+    private void OnGrab(SelectEnterEventArgs args){
+        if(isFadingOut == true){
+            isFadingOut = false;
+            elapsedTime = 0f;
+            color.a = 1f;
+            renderer.material.color = color;
+        }
+    }
 }
