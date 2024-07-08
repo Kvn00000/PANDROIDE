21d56ed8f519e00819da327d2c102de2418c35c7
FirionSun <ewokthr@outlook.fr>
2024-07-08 12:06:16 +0100

    opti counterG2.cs

--- Assets/FinalScene/Scripts/Boids/counterG2.cs
+++ Assets/FinalScene/Scripts/Boids/counterG2.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class counterG2 : MonoBehaviour
 {
+
+    private int _layerSol = LayerMask.NameToLayer("SOL");
+    private int _layerMur = LayerMask.NameToLayer("MUR");
+    private boidTuning parent;
+    private void Start()
+    {
+       parent= transform.parent.GetComponent<boidTuning>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log("Ground encoutered");
         //Debug.Log("FOUND A " + other.gameObject.layer);
         //Debug.Log("LOOKING FOR A " + LayerMask.NameToLayer("SOL") + " OR A " + LayerMask.NameToLayer("MUR"));
-        if ((other.gameObject.layer == LayerMask.NameToLayer("SOL"))||(other.gameObject.layer == LayerMask.NameToLayer("MUR")))
+        if ((other.gameObject.layer ==_layerSol )||(other.gameObject.layer == _layerMur))
         {
             //Debug.Log("FOUND A SOL");
-            boidTuning parent = (boidTuning)transform.parent.GetComponent<boidTuning>();
             parent.AddCollider(other);
             //Debug.Log(other.gameObject.layer + " ADDED");
         }
@@ -20,15 +27,13 @@ public class counterG2 : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if ((other.gameObject.layer == LayerMask.NameToLayer("SOL")) || (other.gameObject.layer == LayerMask.NameToLayer("MUR")))
+        if ((other.gameObject.layer == _layerSol) || (other.gameObject.layer == _layerMur))
         {
-            boidTuning parent = (boidTuning)transform.parent.GetComponent<boidTuning>();
             parent.AddCollider(other);
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        boidTuning parent = (boidTuning)transform.parent.GetComponent<boidTuning>();
         parent.RemoveCollider(other, other.gameObject.layer);
     }
 
